9b0a929332efea1fdfbbb4c7519f7900380d9362
776356314@qq.com <776356314@qq.com>
2019-07-13 22:47:55 +0800

    md

--- src/类/object.kt
+++ src/类/object.kt
@@ -3,6 +3,8 @@ package 类
 /**
  * 对象声明
  * 如果一个类是工具类，里面全是静态方法，此时我们可以使用对象声明；
+ * object声明一个类时，该类自动成为一个简单的单例模式
+ * object声明的类，无法在外部用new的方式重新实例化
  */
 object Utils {
     const val BASE_URL = "http://www.baidu.com/"
--- src/类/类的修饰符.kt
+++ src/类/类的修饰符.kt
@@ -25,3 +25,16 @@ package 类
 //如果一个类是工具类，里面全是静态方法，此时我们可以使用对象object声明；
 //如果一个类里面有部分静态成员（包括静态变量和方法），此时就可以使用 companion object
 
+
+//kotlin学习笔记：object关键字介绍与java中的静态变量与静态方法的实现以及@JvmField和@JvmStatic的使用
+//https://blog.csdn.net/liyi1009365545/article/details/84064326
+
+//@JvmField消除了变量的getter与setter方法
+//@JvmField修饰的变量不能是private属性的
+//@JvmStatic只能在object类或者伴生对象companion object中使用，而@JvmField没有这些限制
+//@JvmStatic一般用于修饰方法，使方法变成真正的静态方法；如果修饰变量不会消除变量的getter与setter方法，但会使getter与setter方法和变量都变成静态
+
+//const只能修饰常量val
+//const只能在object类中或者伴生对象companion object中使用
+//const的效果等于@JvmField，两者不能同时使用
+
